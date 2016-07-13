eee08f849b8aa44a8a865bc8bb8913bfd79333c2
이지윤 <shy817@naver.com>
2016-07-13 12:08:46 +0900

    random select scene create
    draw random box
    create button - click success
    random select ships on the box's position
    
    //not to compare codition(ex. money)

---
+++ Assets/CS/RandomSelectButton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSelectButton : MonoBehaviour {
+
+    //ship arr
+    public GameObject[] ShipPrefabs;
+    public GameObject ship;
+
+	public void OnStartButtonClicked()
+    {
+        CreateShip();
+    }
+
+    public GameObject SelectShip()
+    {
+        GameObject prefab = null;
+        int index = Random.Range(0, ShipPrefabs.Length);
+        prefab = ShipPrefabs[index];
+
+        return prefab;
+    }
+
+    public void CreateShip()
+    {
+        //destroy randombox
+        GameObject box = GameObject.FindGameObjectWithTag("RandomBox");
+        Destroy(box);
+
+        //create the ship
+        ship = (GameObject)Instantiate(SelectShip(), new Vector3(120, 253, 0), Quaternion.identity);
+        ship.tag = "RandomBox";
+        //set scale
+        Vector3 scale = ship.transform.localScale;
+        scale.x *= 30;
+        scale.y *= 30;
+        scale.z *= 30;
+
+        ship.transform.localScale = scale;
+
+    }
+
+    void Update()
+    {
+       //freeze position
+       Rigidbody rigidShip = ship.GetComponent<Rigidbody>();
+       Destroy(rigidShip);
+    }
+}
