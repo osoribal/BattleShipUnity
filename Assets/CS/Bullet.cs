41f41dace5acfce1c2f6101f2b7dea0678e59726
이지윤 <shy817@naver.com>
2016-08-05 23:59:43 +0900

    Bullet gives turn

--- Assets/CS/Bullet.cs
+++ Assets/CS/Bullet.cs
@@ -7,6 +7,14 @@ public class Bullet : MonoBehaviour {
     public Transform to;
     public float value = 10.0F;
     private float startTime;
+    public GameControler gameController;
+
+    //turn
+    public const int USER_TURN = 0;
+    public const int AI_TURN = 1;
+
+    public const int USER_BLOCK = -1;
+    public const int AI_BLOCK = -2;
 
     // Use this for initialization
     void Start () {
@@ -32,15 +40,76 @@ public class Bullet : MonoBehaviour {
     //destroy bullet
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Arrow")
-            Destroy(other.gameObject);
-        if (other.gameObject.tag == "Tile")
+
+        switch (other.gameObject.tag)
         {
-            //remove bullet
-           // Destroy(this.gameObject);
-            //test : remove fog
-            FogControler fg = other.GetComponent<FogControler>();
-            fg.fogOff();
+            case "Arrow":
+                Destroy(other.gameObject);
+                break;
+
+            case "Tile":
+                //remove bullet
+                Destroy(this.gameObject);
+                //test : remove fog
+                FogControler fg = other.GetComponent<FogControler>();
+                fg.fogOff();
+
+                //no hit - change turn
+                ChangeTurn();
+                destroyBullet();
+                break;
+
+            case "Ship":
+                //hit - not change turn, attack again
+                AttackAgain();
+                destroyBullet();
+                break;
         }
     }
+
+    void destroyBullet()
+    {
+        //destroy bullet game object
+        Destroy(this.gameObject);
+    }
+
+    //no hit - change turn
+    void ChangeTurn()
+    {
+        //get game controller's turn
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameControler>();
+        int whoseTurn = gameController.GetTurn();
+
+        //change turn each case
+        switch (whoseTurn)
+        {
+            case USER_BLOCK:
+                gameController.turn = AI_TURN;
+                break;
+            case AI_BLOCK:
+                gameController.turn = USER_TURN;
+                break;
+        }
+    }
+
+    //hit - not change turn, attack again
+    void AttackAgain()
+    {
+        //get game controller's turn
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameControler>();
+        int whoseTurn = gameController.GetTurn();
+
+        //change turn each case
+        switch (whoseTurn)
+        {
+            case USER_BLOCK:
+                gameController.turn = USER_TURN;
+                break;
+            case AI_BLOCK:
+                gameController.turn = AI_TURN;
+                break;
+        }
+    }
+
+
 }
--- Assets/CS/BulletDestroyer.cs
+++ Assets/CS/BulletDestroyer.cs
@@ -4,14 +4,9 @@ using System.Collections;
 public class BulletDestroyer : MonoBehaviour
 {
 
-    public GameControler gameController;
 
-    //ai turn
-    public const int USER_TURN = 0;
-    public const int AI_TURN = 1;
 
-    public const int USER_BLOCK = -1;
-    public const int AI_BLOCK = -2;
+
     
     // Use this for initialization
     void Start()
@@ -27,29 +22,9 @@ public class BulletDestroyer : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet") Destroy(other.gameObject);
-        OnNotify();
+       
     }
 
-    void OnNotify()
-    {
-        //notify to game controller
-        gameController = GameObject.FindWithTag("GameController").GetComponent<GameControler>();
-        int whoseTurn = gameController.GetTurn();
 
-        switch (whoseTurn)
-        {
-            case USER_TURN:
-                break;
-            case AI_TURN:
-                break;
-            case USER_BLOCK:
-                gameController.turn = AI_TURN;
-                break;
-            case AI_BLOCK:
-                gameController.turn = USER_TURN;
-                break;
-
-        }
-    }
 }
     
