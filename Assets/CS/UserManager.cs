297b0c25d58a6ae86a173ef9197f80b025176d6c
osoribal <osoribal@naver.com>
2016-08-08 21:38:40 +0900

    update UserManager, TitleController
    보유골드의 저장, 획득, 사용 함수 구현
    title 씬에서 보유골드 표시

--- Assets/CS/UserManager.cs
+++ Assets/CS/UserManager.cs
@@ -5,24 +5,16 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-
-
 /**** https://www.youtube.com/watch?v=J6FfcJpbPXE&feature=youtu.be *****/
 
-
+//save and load datas
 public class UserManager : MonoBehaviour {
     string path;
     public static UserManager control;
     public static List<ShipInfo> list = new List<ShipInfo>();   //보유중인 배 list
 
-    /*
-     * save/load controler
-     * 골드 저장
-     * 골드 사용/획득 함수 구현
-     * 골드 보유 ui -> title, random scene
-     * 
-     */
-    
+
+    //씬이 변경될 때 UserManager가 유일하도록 유지
     void Awake()
     {
         if (control == null)
@@ -39,23 +31,49 @@ public class UserManager : MonoBehaviour {
 
     void Start()
     {
-        path = Application.persistentDataPath + "/shiplist.dat";
+        path = Application.persistentDataPath + "/userdata.dat";
 
-        //test data
-        this.Save(new ShipInfo(1));
-        this.Save(new ShipInfo(2));
-        this.Save(new ShipInfo(3));
-
+        //데이터 불러오기
         this.Load();
-        
+
+        //PlayerPrefs 초기화
+        //PlayerPrefs.DeleteAll();
+    }
+    
+    //골드 획득/사용 시 호출
+    //파라미터 : 골드 획득/사용 시 변동 값
+    public void updateGold(int g)
+    {
+        int gold = PlayerPrefs.GetInt("gold");
+        gold += g;
+        PlayerPrefs.SetInt("gold", gold);   //gold 값 파일로 저장
+        //다른 씬에서는 PlayerPrefs.GetInt("gold")를 통해 gold 값을 불러올 수 있다.
     }
 
-    public void Save(ShipInfo data)
+    //새로운 배를 저장하는 함수
+    public void SaveShip(ShipInfo data)
     {
-        //보유개수 설정, 저장
-        list.Add(data);
-        //정렬 
+        //보유중인 배인지 Find 함수를 통해 알아낸다.
+        ShipInfo findResult = list.Find(x => x.shipNum.Equals(data.shipNum));
+        if (findResult == null) //미보유
+        {
+            list.Add(data); //추가
+            list.Sort();    //정렬
+        }
+        else
+        {
+            //보유중 : 보유개수만 증가
+            findResult.count++;
+        }
 
+        //파일로 저장
+        Save();
+    }
+
+    //현재 배 정보들을 파일에 저장하는 함수
+    void Save()
+    {
+        //파일로 저장
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Create(path);
         
@@ -63,7 +81,8 @@ public class UserManager : MonoBehaviour {
         file.Close();
     }
 
-    public void Load()
+    //파일로 저장된 배 정보들을 불러오는 함수
+    void Load()
     {
         if (File.Exists(path))
         {
@@ -77,11 +96,8 @@ public class UserManager : MonoBehaviour {
 }
 
 [Serializable]
-public class ShipInfo
+public class ShipInfo : IComparable<ShipInfo>
 {
-    /*
-     * 고유번호만 저장
-     */
     public int shipNum; //고유번호
     public int count;   //보유개수
 
@@ -89,4 +105,16 @@ public class ShipInfo
     {
         shipNum = n;
     }
+
+    //sort 시 비교함수
+    public int CompareTo(ShipInfo comparePart)
+    {
+        // A null value means that this object is greater.
+        if (comparePart == null)
+            return 1;
+
+        else
+            return this.shipNum.CompareTo(comparePart.shipNum);
+    }
+
 }
