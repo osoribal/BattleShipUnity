13bc4946842f8ebdb36d488dbb2b30fa3e958305
osoribal <osoribal@naver.com>
2016-08-22 21:08:39 +0900

    update UserControler.cs, ListElement design
    fix bug occured when arrow is null

--- Assets/CS/UserControler.cs
+++ Assets/CS/UserControler.cs
@@ -27,65 +27,69 @@ public class UserControler : MonoBehaviour {
     
     public IEnumerator shot()
     {
-        //드래그 중
-        GameObject arrow = null;    //맞을 지점을 표시할 오브젝트
-        Vector3 scrSpace = Camera.main.WorldToScreenPoint(transform.position);
-        while (Input.GetMouseButton(0))
+        while (true)
         {
-            /******Aim******/
-            Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, scrSpace.z);
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace);
-            //맞을 지점의 좌표
-            Vector3 position = new Vector3((int)curPosition.x, 2.0f, (int)curPosition.z);
-            //ai 격자 내에만 가능하도록 범위 제한
-            if ( position.x > -11 && position.x <= -1 && position.z >= -5 && position.z < 5)
+            //드래그 중
+            GameObject arrow = null;    //맞을 지점을 표시할 오브젝트
+            Vector3 scrSpace = Camera.main.WorldToScreenPoint(transform.position);
+            while (Input.GetMouseButton(0))
             {
-                position.x -= 1;    //arrow가 손가락에 가려지지 않도록
-                if(arrow != null)
+                /******Aim******/
+                Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, scrSpace.z);
+                Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace);
+                //맞을 지점의 좌표
+                Vector3 position = new Vector3((int)curPosition.x, 2.0f, (int)curPosition.z);
+                //ai 격자 내에만 가능하도록 범위 제한
+                if (position.x > -11 && position.x <= -1 && position.z >= -5 && position.z < 5)
                 {
-                    //기존 지점의 오브젝트 삭제
-                    Destroy(arrow);
+                    position.x -= 1;    //arrow가 손가락에 가려지지 않도록
+                    if (arrow != null)
+                    {
+                        //기존 지점의 오브젝트 삭제
+                        Destroy(arrow);
+                    }
+                    //좌표에 arrow 오브젝트 생성
+                    arrow = (GameObject)Instantiate(arrowPrefab, position, Quaternion.identity);
                 }
-                //좌표에 arrow 오브젝트 생성
-                arrow = (GameObject)Instantiate(arrowPrefab, position, Quaternion.identity);
+
+                yield return null;
+
             }
 
-            yield return null;
-
-        }
-
-        if (arrow != null)
-        {
-            //탄환 생성
-            GameObject bullet = (GameObject)Instantiate(bulletPrefab, new Vector3(2, 1, 0), Quaternion.identity);
-            //탄환 코드에 변수값 전달 -> 탄환 스스로 발사
-            Bullet bc = bullet.GetComponent<Bullet>();
-            Vector3 buf = gc.shipObjs[turn].transform.position;
-            buf.y += 1;
-            bc.from = buf;
-            bc.to = arrow.transform.position;
-            //두 발 쏘는 특수기능 처리
-            if (gc.ships[turn].shipID % 10 == 3)   //두 발 쏘기
+            if (arrow != null)
             {
-                if (firstHit == true)
+                //탄환 생성
+                GameObject bullet = (GameObject)Instantiate(bulletPrefab, new Vector3(2, 1, 0), Quaternion.identity);
+                //탄환 코드에 변수값 전달 -> 탄환 스스로 발사
+                Bullet bc = bullet.GetComponent<Bullet>();
+                Vector3 buf = gc.shipObjs[turn].transform.position;
+                buf.y += 1;
+                bc.from = buf;
+                bc.to = arrow.transform.position;
+                //두 발 쏘는 특수기능 처리
+                if (gc.ships[turn].shipID % 10 == 3)   //두 발 쏘기
                 {
-                    firstHit = false;
-                    bc.turnChange = false;
+                    if (firstHit == true)
+                    {
+                        firstHit = false;
+                        bc.turnChange = false;
+                    }
+                    else
+                    {
+                        firstHit = true;
+                        bc.turnChange = true;
+                        changeTurn();
+                    }
                 }
                 else
                 {
-                    firstHit = true;
                     bc.turnChange = true;
                     changeTurn();
                 }
+                break;
             }
-            else
-            {
-                bc.turnChange = true;
-                changeTurn();
-            }
+            yield return null;
         }
-
     }
 
     void changeTurn()
