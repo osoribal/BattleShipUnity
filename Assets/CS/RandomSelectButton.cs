82494dfce96cd72cc05f03561b05dc16307572e3
이지윤 <shy817@naver.com>
2016-08-23 02:17:49 +0900

    random select scene
    design, gold state update, random select, show result finish
    error - userManager.Save(newShip) has null point if after back to main

--- Assets/CS/RandomSelectButton.cs
+++ Assets/CS/RandomSelectButton.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RandomSelectButton : MonoBehaviour {
 
@@ -8,54 +9,83 @@ public class RandomSelectButton : MonoBehaviour {
     public GameObject[] ShipPrefabs;
     public GameObject ship;
     ShipInfo newShipInfo;
+    public Text textGold;
+    public Text textResult;
+    public Text textMessage;
+
 
     //user manager
     public UserManager userManager;
 
     //user gold info
     int gold;
+    int index;
+    int opnum;
+    string option;
+    int size;
 
-	public void OnStartButtonClicked()
-    {
+    void Start() {
         //get current money
-		gold = PlayerPrefs.GetInt("gold");
+        
+        PlayerPrefs.SetInt("gold", 10000);
+        gold = PlayerPrefs.GetInt("gold");
+        textGold.text = "보유 골드 : " + gold + "G";
+        print("보유 골드 : " + gold + "G");
 
+        //text result, message init to space
+        textResult.text = " ";
+        textMessage.text = " ";
+        
+    }
+
+    public void OnStartButtonClicked()
+    {
         //check current money - equal or greater than 1000 Gold
         if (gold >= 1000)
         {
             //create random ship
             CreateShip();
-            //decrease gold
-            gold = gold - 1000;
-            //save gold
-            //save the new ship
-
-            userManager.Save(newShipInfo);
-
             //alert message
             //the new ship saving is success
 
-			//gold decrease save
-			PlayerPrefs.SetInt("gold", gold);
+            //gold decrease save
+            //PlayerPrefs.SetInt("gold", gold);
+            userManager.updateGold(-1000);
+            //get current money - update state
+            gold = PlayerPrefs.GetInt("gold");
+            alertSelectResult();
         }
         else {
             //alert message - you can't buy a ship.
+            alertCantBuy();
         }
     }
 
-    public GameObject SelectShip()
-    {
-        //create random number
-        GameObject prefab = null;
-        int index = Random.Range(0, ShipPrefabs.Length);
-        //get ship prefab at index
-        prefab = ShipPrefabs[index];
+    void alertSelectResult() {
+        textGold.text = "보유 골드 : " + gold + "G";
+        textResult.text = "축하합니다!";
+        size = index + 1;
+        switch (opnum)
+        {
+            case 2:
+                option = "동귀어진";
+                break;
+            case 3:
+                option = "두번쏘기";
+                break;
+            case 4:
+                option = "골드보상 업";
+                break;
+            default:
+                option = "없음";
+                break;
+        }
+        textMessage.text = "특수능력 :" + option + " / 크기 : " + size;
+    }
 
-        //save new ship info
-        //////////change!!!!! - index -> prefabs.shipID
-		newShipInfo = new ShipInfo(index);
-
-        return prefab;
+    void alertCantBuy() {
+        textResult.text = "골드가 모자랍니다!";
+        textMessage.text = "게임에서 승리하여 골드를 모아주세요.";
     }
 
     public void CreateShip()
@@ -74,14 +104,34 @@ public class RandomSelectButton : MonoBehaviour {
         scale.z *= 30;
 
         ship.transform.localScale = scale;
-        
-
     }
 
-	void Start()
-	{
-		
-	}
+    public GameObject SelectShip()
+    {
+        //create random number
+        GameObject prefab = null;
+        //random size
+        index = Random.Range(0, ShipPrefabs.Length);
+        size = index + 1;
+        
+        //random option
+        opnum = Random.Range(0, 5);
+
+        //get ship prefab at index
+        prefab = ShipPrefabs[index];
+
+        //save new ship info
+        //ship number
+        int shipNum = (10 * size) + opnum;
+		newShipInfo = new ShipInfo(shipNum);
+
+        //save the new ship
+        userManager.Save(newShipInfo);
+
+        return prefab;
+    }
+
+
 
     void Update()
     {
