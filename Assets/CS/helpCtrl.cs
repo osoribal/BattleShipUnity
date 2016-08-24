9056c615cf0314196cc736bcb035601f916ddbbc
osoribal <osoribal@naver.com>
2016-08-24 23:06:08 +0900

    design button prefab
    design button prefab

---
+++ Assets/CS/helpCtrl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class helpCtrl : MonoBehaviour {
+    public Texture[] images;
+    int now;
+
+	// Use this for initialization
+	void Start () {
+        now = 0;
+        gameObject.GetComponent<RawImage>().texture = images[now];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
